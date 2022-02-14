66f26e114fb2f0407b63e0097d74baa82c24c206
mfmoreira <mfmoreira7@gmail.com>
2022-02-14 10:08:24 -0300

    Tests update

--- src/test/java/com/picpay/wallet/UserWalletPicpayApplicationTests.java
+++ src/test/java/com/picpay/wallet/UserWalletPicpayApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class UserWalletPicpayApplicationTest {
+public class UserWalletPicpayApplicationTests {
 
 	@Test
 	void contextLoads() {
